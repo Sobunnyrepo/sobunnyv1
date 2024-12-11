95f74bb28b81f8ee680937c9a90eac57b7f33eeb
Alfredo Gutierrez <alfredo.gutierrez.92@hotmail.com>
2024-12-10 20:32:21 -0600

    ataques

--- core/Modules/Membership/resources/views/frontend/user/membership/membership.blade.php
+++ core/Modules/Membership/resources/views/frontend/user/membership/membership.blade.php
@@ -158,7 +158,7 @@
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="newModalLabel">NOW PAYMENTS</h5>
+                            <h5 style="color:white" class="modal-title" id="newModalLabel">NOW PAYMENTS</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body" style="color:white">
